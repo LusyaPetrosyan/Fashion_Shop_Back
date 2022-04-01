a99c1252d56874ea5f29198fcc8d9ac00b8f82cc
LusyaPetrosyan <96180298+LusyaPetrosyan@users.noreply.github.com>
2022-04-02 01:47:35 +0400

    images crud

--- src/main/java/com/example/fashionshop/repository/ProductRepository.java
+++ src/main/java/com/example/fashionshop/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.example.fashionshop.repository;
 
 import com.example.fashionshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
